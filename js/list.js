50656be6c051daf682b1350e623c543e178fd481
Roberta Pennisi <92723769+RobertaPennisi@users.noreply.github.com>
2021-11-15 17:49:14 +0100

    clean code

--- js/list.js
+++ js/list.js
@@ -2,18 +2,10 @@ import { render } from "./data.js"
 
 const list = (data) => {
     const contents = data
-        .map(item => `<div class="row"><input type="checkbox" name="${item.id}" id="${item.id}"><label for="${item.id}">${item.title}</label></div>` )
+        .map(item => `<div class="row"><input type="checkbox" name="${item.id}" id="${item.id}"${item.completed ? "checked" : ""}><label for="${item.id}">${item.title}</label></div>` )
         .join('');
     const container = document.querySelector('#container');
     render(container, `<form>${contents}</form>`)
 }
 
 export {list}
-
-
-// {
-//     if (item.completed) {
-//         `<label for="${item.id}"></label>${item.title}<input type="checkbox" name="${item.id}" id="${item.id}" checked>` 
-//     } else {
-//         `<label for="${item.id}"></label>${item.title}<input type="checkbox" name="${item.id}" id="${item.id}">`
-//     }}
